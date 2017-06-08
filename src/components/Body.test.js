5c3617dd07dc188301664dab772817b2249c87f3
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2017-06-08 18:23:55 +0100

    Add an extra body test

--- src/components/Body.test.js
+++ src/components/Body.test.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 
 import Body from './Body';
 
 it('mounts without crashing', () => {
   mount(<Body />);
+});
+
+it('renders its children', () => {
+  const child = <div>A test child!</div>;
+  const wrapper = shallow(<Body>{child}</Body>);
+
+  expect(wrapper).toContainReact(child);
 });
