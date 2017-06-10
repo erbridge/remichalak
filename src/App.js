c71d4864225a37c61c399cb094045745ea61ddd8
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2017-06-10 01:38:28 +0100

    Set the page title via the router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import { matchPath } from 'react-router';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
@@ -14,6 +15,18 @@ const topRoutesWithSections = topRoutes.filter(({ section }) => section);
 const App = () =>
   <Router>
     <div className="App">
+      {topRoutes.map((route, index) =>
+        <Route
+          key={index}
+          path={route.path}
+          exact={route.exact}
+          strict={route.strict}
+          render={() =>
+            <Helmet>
+              <title>{route.title ? `${route.title} | ` : ''}remichalak</title>
+            </Helmet>}
+        />,
+      )}
       <Header />
       <Body>
         {topRoutesWithSections.map((route, index) =>
@@ -24,13 +37,17 @@ const App = () =>
             strict={route.strict}
             children={({ location, match }) =>
               <ExpandingSection
-                delayNextAnimation={Boolean(topRoutesWithSections.find(r =>
-                  r.path !== route.path && matchPath(location.pathname, {
-                    path: r.path,
-                    exact: r.exact,
-                    strict: r.strict,
-                  }),
-                ))}
+                delayNextAnimation={Boolean(
+                  topRoutesWithSections.find(
+                    r =>
+                      r.path !== route.path &&
+                      matchPath(location.pathname, {
+                        path: r.path,
+                        exact: r.exact,
+                        strict: r.strict,
+                      }),
+                  ),
+                )}
                 expand={Boolean(match)}
               >
                 <route.section />
