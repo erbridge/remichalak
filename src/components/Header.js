cd062811532d9f516954557019a8ba36e62f52f4
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2017-06-12 16:05:18 +0100

    Make the header highlight the active route

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import classnames from 'classnames';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import { topRoutes } from '../routes';
 
@@ -13,33 +13,39 @@ const Header = () =>
     <h1>remichalak</h1>
     <h2>artist</h2>
     <div className="Header__nav">
-      {topRoutes.map(({ external, link, name, newTab, path }, index) => {
-        const LinkComponent = external ? ExternalLink : Link;
+      {topRoutes.map(
+        ({ exact, external, link, name, newTab, path, strict }, index) => {
+          const LinkComponent = external ? ExternalLink : NavLink;
 
-        const extraLinkProps = {};
+          const extraLinkProps = {};
 
-        if (external) {
-          extraLinkProps.newTab = newTab;
-        }
+          if (external) {
+            extraLinkProps.newTab = newTab;
+          } else {
+            extraLinkProps.activeClassName = 'Header__nav-link__anchor--active';
+            extraLinkProps.exact = exact;
+            extraLinkProps.strict = strict;
+          }
 
-        return (
-          <div
-            key={index}
-            className={classnames(
-              'Header__nav-link',
-              `Header__nav-link--${name}`,
-            )}
-          >
-            <LinkComponent
-              className="Header__nav-link__anchor"
-              to={path}
-              {...extraLinkProps}
+          return (
+            <div
+              key={index}
+              className={classnames(
+                'Header__nav-link',
+                `Header__nav-link--${name}`,
+              )}
             >
-              {link}
-            </LinkComponent>
-          </div>
-        );
-      })}
+              <LinkComponent
+                className="Header__nav-link__anchor"
+                to={path}
+                {...extraLinkProps}
+              >
+                {link}
+              </LinkComponent>
+            </div>
+          );
+        },
+      )}
     </div>
   </div>;
 
