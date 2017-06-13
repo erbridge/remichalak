63ee8b3a30c677decb8f8f6bb04c8153138f58ef
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2017-06-13 21:15:42 +0100

    Set up a lightbox for viewing project images

--- src/components/ProjectPage.js
+++ src/components/ProjectPage.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import Lightbox from 'react-images';
 
 import MarkdownPage from './MarkdownPage';
 
@@ -12,8 +13,60 @@ class ProjectPage extends Component {
     title: PropTypes.string.isRequired,
   };
 
+  state = {
+    currentLightboxImageIndex: 0,
+    lightboxIsOpen: false,
+  };
+
+  openLightbox(index) {
+    this.setState({ currentLightboxImageIndex: index, lightboxIsOpen: true });
+  }
+
+  closeLightbox() {
+    this.setState({ currentLightboxImageIndex: 0, lightboxIsOpen: false });
+  }
+
+  goToNextLightboxImage() {
+    const { images } = this.props;
+    const { currentLightboxImageIndex, lightboxIsOpen } = this.state;
+
+    if (!lightboxIsOpen) {
+      return;
+    }
+
+    this.setState({
+      currentLightboxImageIndex:
+        (currentLightboxImageIndex + 1) % images.length,
+    });
+  }
+
+  goToPreviousLightboxImage() {
+    const { images } = this.props;
+    const { currentLightboxImageIndex, lightboxIsOpen } = this.state;
+
+    if (!lightboxIsOpen) {
+      return;
+    }
+
+    this.setState({
+      currentLightboxImageIndex:
+        (images.length + currentLightboxImageIndex - 1) % images.length,
+    });
+  }
+
+  goToLightboxImage(index) {
+    const { lightboxIsOpen } = this.state;
+
+    if (!lightboxIsOpen) {
+      return;
+    }
+
+    this.setState({ currentLightboxImageIndex: index });
+  }
+
   render() {
     const { content, images, title } = this.props;
+    const { currentLightboxImageIndex, lightboxIsOpen } = this.state;
 
     return (
       <div className="ProjectPage">
@@ -24,10 +77,21 @@ class ProjectPage extends Component {
               className="ProjectPage__image"
               src={src}
               alt={title}
+              onClick={() => this.openLightbox(index)}
             />,
           )}
         </div>
         <MarkdownPage content={content} />
+        <Lightbox
+          images={images.map(src => ({ src }))}
+          currentImage={currentLightboxImageIndex}
+          isOpen={lightboxIsOpen}
+          showThumbnails
+          onClickNext={() => this.goToNextLightboxImage()}
+          onClickPrev={() => this.goToPreviousLightboxImage()}
+          onClickThumbnail={index => this.goToLightboxImage(index)}
+          onClose={() => this.closeLightbox()}
+        />
       </div>
     );
   }
