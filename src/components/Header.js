025c1ddcf2b1e332d3f4c5e33597364c053515da
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2017-06-14 14:52:34 +0100

    Wrap the header in a content div

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,45 +10,48 @@ import './Header.css';
 
 const Header = () =>
   <div className="Header">
-    <h1>remichalak</h1>
-    <h2>artist</h2>
-    <div className="Header__nav">
-      {topRoutes.map(
-        (
-          { exact, external, link, name, navExact, newTab, path, strict },
-          index,
-        ) => {
-          const LinkComponent = external ? ExternalLink : NavLink;
+    <div className="Header__content">
+      <h1>remichalak</h1>
+      <h2>artist</h2>
+      <div className="Header__nav">
+        {topRoutes.map(
+          (
+            { exact, external, link, name, navExact, newTab, path, strict },
+            index,
+          ) => {
+            const LinkComponent = external ? ExternalLink : NavLink;
 
-          const extraLinkProps = {};
+            const extraLinkProps = {};
 
-          if (external) {
-            extraLinkProps.newTab = newTab;
-          } else {
-            extraLinkProps.activeClassName = 'Header__nav-link__anchor--active';
-            extraLinkProps.exact = exact && navExact;
-            extraLinkProps.strict = strict;
-          }
+            if (external) {
+              extraLinkProps.newTab = newTab;
+            } else {
+              extraLinkProps.activeClassName =
+                'Header__nav-link__anchor--active';
+              extraLinkProps.exact = exact && navExact;
+              extraLinkProps.strict = strict;
+            }
 
-          return (
-            <div
-              key={index}
-              className={classnames(
-                'Header__nav-link',
-                `Header__nav-link--${name}`,
-              )}
-            >
-              <LinkComponent
-                className="Header__nav-link__anchor"
-                to={path}
-                {...extraLinkProps}
+            return (
+              <div
+                key={index}
+                className={classnames(
+                  'Header__nav-link',
+                  `Header__nav-link--${name}`,
+                )}
               >
-                {link}
-              </LinkComponent>
-            </div>
-          );
-        },
-      )}
+                <LinkComponent
+                  className="Header__nav-link__anchor"
+                  to={path}
+                  {...extraLinkProps}
+                >
+                  {link}
+                </LinkComponent>
+              </div>
+            );
+          },
+        )}
+      </div>
     </div>
   </div>;
 
