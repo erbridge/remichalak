f80d4191899a2d6b11e9f9fc08fb264dc128e2a9
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2017-06-19 17:12:50 +0100

    Use the erbridge logo in the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import erbridgeLogo from '../assets/misc/erbridge-logo.svg';
+
 import ExternalLink from './ExternalLink';
 
 import './Footer.css';
@@ -9,7 +11,13 @@ const Footer = () =>
     <div className="Footer__content">
       <div className="Footer__credit">
         built by{' '}
-        <ExternalLink to="http://erbridge.co.uk/">erbridge</ExternalLink>
+        <ExternalLink to="http://erbridge.co.uk/">
+          <img
+            className="Footer__credit-logo"
+            src={erbridgeLogo}
+            alt="erbridge"
+          />
+        </ExternalLink>
       </div>
     </div>
   </div>;
